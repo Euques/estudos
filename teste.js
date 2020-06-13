45b9a539511c7cf69752ef54bee67ba138cf289c
Euques Wabes <euques@gmail.com>
2020-06-13 17:48:39 -0300

    teste

---
+++ teste.js
@@ -0,0 +1,24 @@
+var funcionarios = 
+    {
+        "Marconildo":
+        {
+            "url": "http://www.marconildo.com.br/",
+            "idade": 34
+        },
+        "Joanorta":
+        {
+            "url": "http://www.joanorta.com.br/",
+            "idade": 22
+        },
+        "Estripafurgia":
+        {
+            "url": "http://www.estripafurgia.com.br/",
+            "idade": 47
+        }
+    }
+;
+ 
+var funcionario = JSON.parse(funcionarios);
+ 
+alert(funcionario.Marconildo);
+alert(funcionario.Marconildo.idade);
